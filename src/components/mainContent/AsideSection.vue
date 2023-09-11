c23f223aa35f3ff9d2349669525fce0f6edfe735
Syderi <syderi@yahoo.com>
2023-09-11 15:38:50 +0300

    feat: seargh list

--- src/components/mainContent/AsideSection.vue
+++ src/components/mainContent/AsideSection.vue
@@ -3,6 +3,11 @@ import UserPrevueCard from './uiComponents/UserPrevueCard.vue';
 import Preloader from '../../ui_global/Preloader.vue';
 
 export default {
+  data() {
+    return {
+      searchQuery: '',
+    };
+  },
   components: {
     UserPrevueCard,
     Preloader,
@@ -15,7 +20,18 @@ export default {
     isLoadingUsers() {
       return this.$store.state.isLoadingUsers;
     },
+    filteredUsers() {
+      const query = this.searchQuery.toLowerCase();
+
+      return this.users.filter((user) => {
+        return (
+          user.name.toLowerCase().includes(query) ||
+          user.id.toString().includes(query)
+        );
+      });
+    },
   },
+
   created() {
     this.fetchUsers();
   },
@@ -30,7 +46,7 @@ export default {
 <template>
   <aside :class="$style.wrapper">
     <h3>Поиск сотрудников</h3>
-    <input type="text" placeholder="Введите Id или имя" />
+    <input type="text" placeholder="Введите Id или имя" v-model="searchQuery" />
     <h3>Результаты</h3>
     <div :class="$style.preloader" v-if="isLoadingUsers">
       <Preloader />
@@ -38,7 +54,7 @@ export default {
       <p>Загрузка данных пользователей...</p>
     </div>
     <ul :class="$style.usersWrapper">
-      <li v-for="user in users" :key="user.id">
+      <li v-for="user in filteredUsers" :key="user.id">
         <UserPrevueCard :name="user.name" :email="user.email" :id="user.id" />
       </li>
     </ul>
